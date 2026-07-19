231028c997af554b5178f066f2eaf57600a38a06
Hari-maker-del <pvhari41@gmail.com>
2026-07-19 15:47:01 +0530

    Time: 3 ms (95.23%), Space: 43.5 MB (57.79%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int[] sCount = new int[10];
+        int[] gCount = new int[10];
+
+        // First pass: count bulls and non-bull frequencies
+        for (int i = 0; i < secret.length(); i++) {
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+
+            if (s == g) {
+                bulls++;
+            } else {
+                sCount[s - '0']++;
+                gCount[g - '0']++;
+            }
+        }
+
+        // Second pass: count cows
+        int cows = 0;
+        for (int d = 0; d <= 9; d++) {
+            cows += Math.min(sCount[d], gCount[d]);
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
