6b303f3fa2789a4cf7b1a808648d617b83a87516
Hari-maker-del <pvhari41@gmail.com>
2026-07-29 16:14:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,20 @@
-public int rob(int[] nums) {
-    return rob(nums, nums.length - 1);
-}
-private int rob(int[] nums, int i) {
-    if (i < 0) {
-        return 0;
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];        
     }
-    return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
 }
